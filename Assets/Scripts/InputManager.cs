5b1113a4a0813b071d9cfcb6afa3beac3ba9c63a
FalseMorels <fals3m0r3ls@gmail.com>
2026-01-31 12:55:08 -0600

    Player Movement & Camera v2
    Adjusted the Player Controller Script
    Added New Input System
    Adjusted Camera - Now first person
    ---Camera is still extremely sensitive, tweaking.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+    private PlayerInputController playerControls;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        playerControls = new PlayerInputController();
+    }
+
+    private void OnEnable()
+    {
+           playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetMovementInput()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetLookInput()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerInteract()
+    {
+        return playerControls.Player.Interact.triggered;
+    }
+
+}
---
+++ Assets/Scripts/PlayerControllerv2.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerv2: MonoBehaviour
+{
+    public CharacterController controller;
+
+    [Header("Variables")]
+    [SerializeField]
+    private float playerSpeed = 5.0f;
+    [SerializeField]
+    private float jumpHeight = 1.5f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    [SerializeField]
+    private Vector3 playerVelocity;
+    [SerializeField]
+    private bool groundedPlayer;
+    [SerializeField]
+    private bool interacted;
+
+    //[SerializeField]; didnt actually need
+    //private InputManager inputManager; didnt actually need
+
+    [Header("Input Actions")]
+    public InputActionReference moveAction;
+    public InputActionReference interactAction;
+    public InputActionReference jumpAction;
+
+    private void Start() //Get the CharacterController component
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        //inputManager = InputManager.Instance; didnt actually need
+    }
+
+    private void OnEnable() //Enable the input actions
+    {
+        moveAction.action.Enable();
+        interactAction.action.Enable();
+    }
+
+    private void OnDisable() //Disable the input actions
+    {
+        moveAction.action.Disable();
+        interactAction.action.Disable();
+    }
+
+    void Update() //Handle movement, jumping and interacting
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer)
+        {
+            // Slight downward velocity to keep grounded stable
+            if (playerVelocity.y < -2f)
+                playerVelocity.y = -2f;
+        }
+
+        // Read input
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        // Jump using WasPressedThisFrame()
+        if (groundedPlayer && jumpAction.action.WasPressedThisFrame())
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Move
+        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
+        controller.Move(finalMove * Time.deltaTime);
+    }
+}
