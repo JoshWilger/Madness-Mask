ed391bb412f3e92a98739cdd7493efe83945cce1
FalseMorels <fals3m0r3ls@gmail.com>
2026-01-31 21:39:02 -0600

    Doors

--- Assets/Scripts/Interaction System/InteractObject.cs
+++ Assets/Scripts/Interaction System/InteractObject.cs
@@ -5,57 +5,60 @@ using UnityEngine.Events;
 
 public class InteractObject : MonoBehaviour, IInteractable
 {
-    //[SerializeField] private string displayName = "Interactable Object";
     [SerializeField] private string Prompt = "Press E to interact";
-    //[SerializeField] private bool isEnabled = true;
-    //[SerializeField] private UnityEvent onInteract;
+
+    [SerializeField] private bool redDoor = false;
+    [SerializeField] private bool redKey = false;
+
+    [SerializeField] private Inventory inv = null;
+
+    private KeyDoorController doorObj;
 
     public string InteractionPrompt => Prompt;
-
-    //public string DisplayName => displayName;
 
     public bool Interact(Interactor interactor)
     {
         Debug.Log(message:"Interact with " + gameObject.name);
 
-        //var inventory = interactor.GetComponent<Inventory>();
 
-        //if (inventory == null) return false;
 
-        /*if (inventory.HasKey) {
-            Debug.Log(message: "You used the key to interact with " + gameObject.name);
-        }
-        else
+
+        //switching between specific objects based on tag
+        string tag = gameObject.tag;
+
+        switch (tag.ToLower())
         {
-            Debug.Log(message: "You need a key to interact with " + gameObject.name);
-            return false;
-        }*/
+            case "redkey":
+                if (redKey)
+                {
+                    inv.HasRedKey = true;
+                    Debug.Log("You picked up a Red Key!");
+                    Destroy(gameObject);
+                }
+                break;
+            case "reddoor":
+                if (redDoor)
+                {
+                    doorObj = GetComponent<KeyDoorController>();
+                    if (inv.HasRedKey)
+                    {
+                        //doorObj.PlayAnimation(); -- not set up yet
+                        Debug.Log("Door Opened");
+                    }
+                    else
+                    {
+                        Debug.Log("You need a Key to open this door.");
+                    }
+                }
+                break;
+            case "":
+                break;
+            default:
+                break;
+        }
 
         return true;
     }
 
-    // private Outline outline;
-    /* private void Awake()
-     {
-         outline.gameObject.AddComponent<Outline>();
-         outline.OutlineMode = Outline.Mode.OutlineVisible;
-         outline.OutlineColor = Color.yellow;
-         outline.OutlineWidth = 3f;
-         outline.enabled = false;
-     }
-     /*public void Interact(Interactor interactor)
-     {
-         onInteract?.Invoke();
-     }
-
-     public void OnFocusGained()
-     {
-         outline.enabled = true;
-     }
-
-     public void OnFocusLost()
-     {
-         outline.enabled = false;
-     }*/
 
 }
--- Assets/Scripts/Interaction System/KeyDoorController.cs
+++ Assets/Scripts/Interaction System/KeyDoorController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyDetector : MonoBehaviour
+public class KeyDoorController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
