2fb58c10ec08f7adce8f378edec634f90d613f8c
JoshWilger <hap3guy@gmail.com>
2026-02-01 00:40:51 -0600

    Transition Fade

--- Assets/Scripts/Interaction System/Interactables/InteractableSceneChanger.cs
+++ Assets/Scripts/Interaction System/Interactables/InteractableSceneChanger.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,13 +6,35 @@ namespace Interaction_System.Interactables
 {
     public class InteractableSceneChanger : InteractObject
     {
-        [SerializeField] public string sceneName;
+        [SerializeField] public string transitionText;
+        [SerializeField] public Animator transitionAnimator;
+        [SerializeField] public TextMeshProUGUI textUi;
+        
+        private float? interactedTime;
         
         public override bool Interact(Interactor interactor)
         {
-            SceneManager.LoadScene(sceneName);
-            
-            return true;
+            if (interactedTime == null)
+            {
+                interactedTime = Time.time;
+                textUi.text = transitionText;
+                transitionAnimator.gameObject.SetActive(true);
+                transitionAnimator.Play("SceneTransition", 0, 0f);
+                Debug.Log(transitionAnimator.gameObject.name + " fade out has begun at " + interactedTime);
+                
+                return true;
+            }
+
+            return false;
+        }
+
+        void LateUpdate()
+        {
+            if (interactedTime + 2 < Time.time && Input.anyKey)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                Debug.Log($"Loading Next Scene at {Time.time}...");
+            }
         }
     }
 }
